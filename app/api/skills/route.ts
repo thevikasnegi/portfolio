cb2bca34d047ad39b94c3ffb7d46982222988ba6
Vikas Negi <72794288+thevikasnegi@users.noreply.github.com>
2025-07-01 12:44:49 +0530

    api comment

--- app/api/skills/route.ts
+++ app/api/skills/route.ts
@@ -2,13 +2,14 @@ import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 export const GET = async (_:Request) => {
     try {
-        const skills = await db.skill.findMany({
-            select:{
-                name: true,
-                level: true,
-            }
-        });
-        return NextResponse.json(skills)
+        // const skills = await db.skill.findMany({
+        //     select:{
+        //         name: true,
+        //         level: true,
+        //     }
+        // });
+        // return NextResponse.json(skills)
+        return NextResponse.json({})
     } catch (error) {
         return new NextResponse("Internal Error", {status: 500})
     }
